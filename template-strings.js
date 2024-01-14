8bde5209bdd0e3cc974b04a8cb465dc4e4e38ebc
Enzo Lima <enzolima527@gmail.com>
2024-01-14 18:39:32 -0300

    .

---
+++ template-strings.js
@@ -0,0 +1,12 @@
+const nome = "enzo";
+const idade = 2024 - 2007;
+const cidadeNascimento = "Rio Branco";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`
+
+console.log(apresentacao)
+
+
+
