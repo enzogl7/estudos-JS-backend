9769d2e555dbdbe2464e8eaf0d4d04d4dea8b14d
Enzo Lima <enzolima527@gmail.com>
2024-01-14 14:02:18 -0300

    .

--- script.js
+++ script.js
@@ -5,16 +5,26 @@
 // console.log(area)
 // var area;
 
-let forma = 'retangulo';
-let altura = 5;
-let comprimento = 7;
+// let forma = 'retangulo';
+// let altura = 5;
+// let comprimento = 7;
+// let area;
 
-if (forma === 'retangulo') {
-    area = altura * comprimento;  
+// if (forma === 'retangulo') {
+//     area = altura * comprimento;  
+// } else {
+   //  area = (altura * comprimento) / 2
+// }
+//console.log(area);
+
+const forma = 'triangulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+if (forma === 'quadrado') {
+    area = altura * comprimento;
 } else {
-    area = (altura * comprimento) / 2
+    area = (altura * comprimento) / 2;
 }
 
-
-console.log(area);
-let area;
+console.log(area)
---
+++ truthy-falsy.js
@@ -0,0 +1,21 @@
+// boolean
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy ou falsy
+// 0 = false
+// 1 = true
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+// null -> dado que representa vazio
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+console.log(typeof minhaVar)
+console.log(typeof varNull)
+// undefined -> campo vazio, sem preencher com nenhum dado
