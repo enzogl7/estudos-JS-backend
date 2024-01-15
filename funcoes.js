921b65261ceffab82ee0239d69890a16a1a77baa
Enzo Lima <enzolima527@gmail.com>
2024-01-15 01:06:59 -0300

    .

---
+++ funcoes.js
@@ -0,0 +1,13 @@
+// let x = "";
+// console.log(x)
+// x = 'oi'
+                     //string
+function imprimeTexto(texto) {
+    console.log(texto)
+
+}
+
+
+// 2) executa a função (1 ou + vezes)
+imprimeTexto("Oi");
+imprimeTexto("outro texto");
