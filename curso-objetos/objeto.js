7d5d8c4c501f2ea1ead97a81de45d0352a7680b3
Enzo Lima <enzolima527@gmail.com>
2024-01-30 17:13:10 -0300

    .

---
+++ curso-objetos/objeto.js
@@ -0,0 +1,11 @@
+const listaCPFs = [1111, 2222, 3333];
+
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "111222333"];
+
+console.log(informacoesPessoa[1])
+
+const objetoPessoa = {
+    nome: "Jose",
+    idade: 32,
+    
+};
