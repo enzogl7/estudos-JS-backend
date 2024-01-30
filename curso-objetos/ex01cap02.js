438cc51e3adff12c9ca8cb001bb0d93293d0c107
Enzo Lima <enzolima527@gmail.com>
2024-01-30 18:05:23 -0300

    .

---
+++ curso-objetos/ex01cap02.js
@@ -0,0 +1,8 @@
+const estudante = {
+    nome: "Enzo Oliveira Galdino Lima",
+    matricula: 55,
+    curso: "Análise e desenvolvimento de sistema",
+    materias: ["Cálculo 1", "C++", "C#"]
+};
+
+console.log(estudante)
