9b7df3d1d6ba5771684e57d7122603ca63e6839f
Enzo Lima <enzolima527@gmail.com>
2024-02-02 17:19:06 -0300

    .

--- curso-algoritmos/menorValor.js
+++ curso-algoritmos/menorValor.js
@@ -1,8 +1,7 @@
 const livros = require("./listaLivros");
 
-function menorValor(arrProdutos, posicaoInicial) {
+function menorValor (arrProdutos, posicaoInicial) {
     let maisBarato = posicaoInicial;
-
     for (let atual = posicaoInicial; atual < arrProdutos.length; atual++){
         if (arrProdutos[atual].preco < arrProdutos[maisBarato].preco) {
             maisBarato = atual;
@@ -10,6 +9,4 @@ function menorValor(arrProdutos, posicaoInicial) {
     };
     return maisBarato;
 }
-
-
-console.log(`O livro mais barato custa ${livros[maisBarato].preco} e seu título é ${livros[maisBarato].titulo}`);
+module.exports = menorValor
--- curso-algoritmos/selectionSort.js
+++ curso-algoritmos/selectionSort.js
@@ -1,5 +1,16 @@
 const livros = require("./listaLivros");
-const menorValor = require("./menorValor");
-for (let atual = 0; atual < livros.length; atual++) {
+const menorValor = require("./menorValor")
+for (let atual = 0; atual < livros.length - 1; atual++){
+    let menor = menorValor(livros, atual)
     
+    let livroAtual = livros[atual];
+    console.log("Posição atual", atual)
+    console.log("Livro atual", livros[atual])
+    let livroMenorPreco = livros[menor];
+    console.log("Livro menor preço", livros[menor])
+
+    livros[atual] = livroMenorPreco
+    livros[menor] = livroAtual
 }
+
+console.log(livros)
